be0dad28f03e026c53120bac64bdcb8f49004288
ASHUTOSH SINGH KUSHWAHA <50301349+ashutoshsinghkushwaha@users.noreply.github.com>
2022-02-08 00:31:07 +0530

    Add files via upload

---
+++ sms mca project/onlinecourse/admin/includes/footer.php
@@ -0,0 +1,10 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    &copy; 2021 Online Course Registration | By : <a href="https://www.google.com/" target="_blank">ASHUTOSH</a>
+                </div>
+
+            </div>
+        </div>
+    </footer>
---
+++ sms mca project/onlinecourse/includes/footer.php
@@ -0,0 +1,13 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    &copy; 2021 school management system  | By : @shutosh singh 
+                </div>
+                <div class="col-md-12">
+                    &copy;  email :-ashutosh.as608@gmail.com | contact: 7784091362/7905701415
+                </div>
+
+            </div>
+        </div>
+    </footer>
